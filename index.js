f4a0380d42c4eff6be54e6aae9068251651aeba6
Eva <145874312+EvaCanary@users.noreply.github.com>
2024-02-05 20:14:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let setButton = document.getElementById('setButton');
+let resetButton = document.getElementById('resetButton') 
+let counterText = document.querySelector('.counter')
+let counter = 0;
+counterText.innerText = counter; 
+
+setButton.addEventListener('click', function() {
+    counter = counter + 1;
+    counterText.innerText = counter;
+});
+
+resetButton.addEventListener('click', function() {
+    counter = 0;
+    counterText.innerText = counter;
+})
+
+// //DOM - Document Object Model
+// comsole.log(document);
